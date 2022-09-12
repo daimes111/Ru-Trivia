43017b7a01f5e9ca26cc448206bcff810f18f5ef
Devon Aimes <aimes.devon@gmail.com>
2022-09-12 16:19:32 -0400

    display updated in medium html with basic structure. Building function to generate question

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
-//Build to show the question and the answer
+console.log("Game Time!")
 
-// variables
-const mediumQuestion = document.querySelector(".medium-question")
-const choicesContainer = document.querySelector(".choices-container")
-const submitBtn = document.getElementById('submit-btn')
+const game = document.getElementById("game")
+const scoreDisplay = document.getElementById("score-display")
+const questionContainer = document.querySelector(".question-container") 
+const answerContainer = document.querySelector(".answer-container") 
+const submitBtn = document.querySelector(".submit-Btn")
+const nextBtn = document.querySelector(".next-Btn")
 
-//functions
-function buildQuestion() {
-    // variable to store the HTML outout
-    const output = [];
-
-    ruQuestions.forEach((currentQuestion, questionNumber) => {
-         //stores the list of possible answers
-         const answers = [];
-
-         //and for each available answer
-         for(letter in currentQuestion.answers){
-
-            //add button
-            answers.push(
-                ''
-            )
-         }
-    })
+function generateQuestion() {
+    const question = document.createElement("h2")
+    questionContainer.appendChild(question)
+    question.innerHTML= ruQuestions[0].question
+    
+    ruQuestions[0].answers.forEach(element => {
+        
+            const answer = document.createElement("p")
+            answerContainer.append(answer)
+            answer.innerHTML= ruQuestions[0].answers
+    
+        
+    });
+    
 }
 
-function showResults() {
-
-}
-
-buildQuestion();
 
 const ruQuestions = [
     {
@@ -51,8 +44,121 @@ const ruQuestions = [
             D: "Raja"
         },
         correctAnswer: "A"
+    }, 
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "Who was the winner of the first season of Ru Paul's Drag Race?",
+        answers: {
+            A: "BeBe Zahara Benet",
+            B: "Nina Flowers",
+            C: "Shangela",
+            D: "Raja"
+        },
+        correctAnswer: "A"
+    }
+]
+generateQuestion()
 
-}]
 
+//first code
+//Build to show the question and the answer
+
+// // variables
+// const mediumQuestion = document.querySelector(".medium-question")
+// const choicesContainer = document.querySelector(".choices-container")
+// const submitBtn = document.getElementById('submit-btn')
+
+// //functions
+// function buildQuestion() {
+//     // variable to store the HTML outout
+//     const output = [];
+
+//     ruQuestions.forEach((currentQuestion, questionNumber) => {
+//          //stores the list of possible answers
+//          const answers = [];
+
+//          //and for each available answer
+//          for(letter in currentQuestion.answers){
+
+//             //add button
+//             answers.push(
+//                 ''
+//             )
+//          }
+//     })
+// }
+
+// function showResults() {
+// }
+// buildQuestion();
 //button event listeners
-submitBtn.addEventListener('click', showResults);
+// submitBtn.addEventListener('click', showResults);
